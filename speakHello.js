2b920c04cde4b5934954939c328759547bce0f2d
Adityabhardwaj2121 <bhardwajaditya67@gmail.com>
2025-06-24 20:41:53 +0530

    Create speakHello.js

---
+++ speakHello.js
@@ -0,0 +1,10 @@
+// Hello Speaker IIFE
+(function (window) {
+  var helloSpeaker = {};
+
+  helloSpeaker.speak = function (name) {
+    console.log("Hello " + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
